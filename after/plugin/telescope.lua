2bbc2c3cb05dccbe6b6fc80b0d69ba20ed5e031c
Hind Sagar Biswas <hindsbhk@gmail.com>
2023-08-21 00:38:32 +0600

    Minor changes

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,16 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-f>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+
+require('telescope').setup({
+  file_ignore_patterns = {
+    'node_modules/*',
+    'vendor/*',
+    '.git/*',
+  },
+})
+
